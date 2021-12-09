c24682ed4da36c2644b8cc1be55776a4d2bf0d97
Ashu Singh <ashu.ask.singh@gmail.com>
2021-12-09 15:47:34 +0530

    Adding "Add new employee" controller

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -41,5 +42,15 @@ public class EmployeeController {
         return "employees/employee-form";
    }
 
+   @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee theEmployee){
+
+        //save the employee
+        employeeService.save(theEmployee);
+
+        //use a redirect to prevent duplicate submission
+        return "redirect:/employees/list";
+
+   }
 
 }
--- src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -3,10 +3,17 @@ package com.luv2code.springboot.thymeleafdemo.dao;
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     //that's it ... no need to write any code LOL!
 
     //no need to write any implementation class
+
+    //add a method to sort by last name
+    //Spring data JPA will automatically understand the below query and return result accordingly
+    // :) How great is that
+    public List<Employee> findAllByOrderByLastNameAsc();
 
 }
